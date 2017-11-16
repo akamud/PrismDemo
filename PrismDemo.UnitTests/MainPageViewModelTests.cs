7c1b558eb216fbb2912424a29ea3aa6b0e120fc8
Mahmoud Ali <muddibr@gmail.com>
2017-11-16 16:01:20 -0300

    Teste

--- PrismDemo.UnitTests/MainPageViewModelTests.cs
+++ PrismDemo.UnitTests/MainPageViewModelTests.cs
@@ -10,18 +10,6 @@ namespace PrismDemo.UnitTests
     [TestFixture]
     public class MainPageViewModelTests
     {
-        [Test]
-        public void LoginCommandShouldShowNavigateToHomePage()
-        {
-            var navigationMock = new Mock<INavigation>();
-
-            var vm = new MainPageViewModel(new HomePage());
-            vm.Username = "Username";
-            vm.Senha = "Senha";
-
-            vm.LoginCommand.Execute(null);
-
-            navigationMock.Verify(x => x.PushAsync(It.Is<Page>(p => p.GetType() == typeof(HomePage))), Times.Once());
-        }
+        
     }
 }
